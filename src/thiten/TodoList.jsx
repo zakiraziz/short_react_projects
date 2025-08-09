0f7bdf51a757b35b45c3a395b5ad21662b5bdcbd
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-08-09 19:59:00 +0500

    Update TodoList.jsx

---
+++ src/thiten/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList({ todos, completeTodo, removeTodo }) {
+  return (
+    <div className="todo-list">
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index}
+          index={index}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
