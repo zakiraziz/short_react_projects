925c71bd8dbf80517f7f97eae7d279e5a24b07ee
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-08-09 19:58:11 +0500

    Update App.jsx

---
+++ src/thiten/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+      />
+    </div>
+  );
+}
+
+export default App;
