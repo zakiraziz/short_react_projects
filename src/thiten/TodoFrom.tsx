87fd24b99c2e5a0bd4cbbbc3c07d96a8c82feb1b
Git Cleeren <yourGitHubID+zakiraziz@users.noreply.github.com>
2025-09-14 22:15:21 +0500

    my new poject

---
+++ src/thiten/TodoFrom.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Add new todo..."
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
