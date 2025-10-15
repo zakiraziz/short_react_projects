1c64b15e262561075d4404812bfd0d96bc4dae44
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-10-15 22:12:12 +0500

    Update BugList.jsx

---
+++ src/eight/context/components/BugList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
