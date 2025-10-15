b551308aad129b2708d2aa36e6d2f4259015d919
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-10-15 22:12:32 +0500

    Update TestCaseList.jsx

---
+++ src/eight/context/components/components/TestCaseList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
