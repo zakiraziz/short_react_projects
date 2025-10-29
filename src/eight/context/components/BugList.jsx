c951175c783bf884094d494c307a875c837f6570
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-10-29 19:41:24 +0500

    Update BugList.jsx

---
+++ src/eight/context/components/BugList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">MyReactApp</h1>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
