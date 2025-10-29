c6a114b8f948572941fa46eba55e9a5fc967d732
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-10-29 19:41:08 +0500

    Update QaContext.jsx

---
+++ src/eight/context/QaContext.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
